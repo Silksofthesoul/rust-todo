c935cfd7028002d434594b4065b1a53e995b9f02
silksofthesoul <silksofthesoul@yandex.ru>
2024-09-17 02:02:13 +0300

    feat(todo): remove methods are added

--- src/todo.rs
+++ src/todo.rs
@@ -25,6 +25,7 @@ pub struct TodoItem {
     title: String,
     created: String,
     ended: String,
+    params: HashMap<String, String>,
 }
 
 #[derive(Deserialize, Debug, Clone)]
@@ -45,6 +46,10 @@ pub struct Todo {
     scanner: Scanner,
     #[serde(skip_serializing, skip_deserializing)]
     renderer: TTR,
+    #[serde(skip_serializing, skip_deserializing)]
+    todoMode: i32, // 0: show all, 1: show done, 2: show undone
+    #[serde(skip_serializing, skip_deserializing)]
+    todoIndexes: Vec<i32>, // -1: show all / default, value greater then -1 -- items for show
 }
 
 impl Todo {
@@ -73,7 +78,29 @@ impl Todo {
             settings,
             scanner,
             renderer,
+            todoMode: 0,
+            todoIndexes: vec![-1],
         }
+    }
+
+    pub fn resetTodoMode(&mut self) -> &mut Self {
+        self.todoMode = 0;
+        self
+    }
+
+    pub fn setTodoMode(&mut self, mode: i32) -> &mut Self {
+        self.todoMode = mode;
+        self
+    }
+
+    pub fn resetTodoIndexes(&mut self) -> &mut Self {
+        self.todoIndexes = vec![-1];
+        self
+    }
+
+    pub fn setTodoIndexes(&mut self, indexes: Vec<i32>) -> &mut Self {
+        self.todoIndexes = indexes;
+        self
     }
 
     fn initProperties(data: String) -> HashMap<String, String> {
@@ -128,6 +155,7 @@ impl Todo {
             title: String::from(title),
             created: String::from("0"),
             ended: String::from(""),
+            params: HashMap::new(),
         };
         self.items.push(todoItem);
         self
@@ -168,30 +196,61 @@ impl Todo {
             TString::new(String::from("Ended")),
         ]);
 
-        for (index, val) in items.iter().enumerate() {
-            let number: &mut TString = &mut TString::new(index.to_string());
-            number.setAnsi(TStringStatic::getForeground("lightGray"));
-            number.setAnsi(TStringStatic::getForeground("lightGray"));
-            let status: TString = if val.status {
-                let status = &mut TString::new("[x]".to_string());
-                status.setAnsi(TStringStatic::getForeground("green"));
-                status.clone()
-            } else {
-                TString::new("[ ]".to_string())
-            };
-            let title = if val.status {
-                let mut title = TString::new(val.title.to_string());
-                title.setAnsi(TStringStatic::getForeground("lightGreen"));
-                title
-            } else {
-                TString::new(val.title.to_string())
-            };
-            let created = TString::new(val.created.to_string());
-            let ended = TString::new(val.ended.to_string());
-            renderer.setRow(vec![number.clone(), status, title, created, ended]);
+        if self.todoIndexes.contains(&(-1 as i32)) {
+            for (index, val) in items.iter().enumerate() {
+                let number: &mut TString = &mut TString::new(index.to_string());
+                number.setAnsi(TStringStatic::getForeground("lightGray"));
+                number.setAnsi(TStringStatic::getForeground("lightGray"));
+                let status: TString = if val.status {
+                    let status = &mut TString::new("[x]".to_string());
+                    status.setAnsi(TStringStatic::getForeground("green"));
+                    status.clone()
+                } else {
+                    TString::new("[ ]".to_string())
+                };
+                let title = if val.status {
+                    let mut title = TString::new(val.title.to_string());
+                    title.setAnsi(TStringStatic::getForeground("lightGreen"));
+                    title
+                } else {
+                    TString::new(val.title.to_string())
+                };
+                let created = TString::new(val.created.to_string());
+                let ended = TString::new(val.ended.to_string());
+                renderer.setRow(vec![number.clone(), status, title, created, ended]);
+            }
+        } else {
+            for (index, val) in items.iter().enumerate() {
+                let number: &mut TString = &mut TString::new(index.to_string());
+                number.setAnsi(TStringStatic::getForeground("lightGray"));
+                match self.todoIndexes.contains(&(index as i32)) {
+                    true => {
+                        let status: TString = if val.status {
+                            let status = &mut TString::new("[x]".to_string());
+                            status.setAnsi(TStringStatic::getForeground("green"));
+                            status.clone()
+                        } else {
+                            TString::new("[ ]".to_string())
+                        };
+                        let title = if val.status {
+                            let mut title = TString::new(val.title.to_string());
+                            title.setAnsi(TStringStatic::getForeground("lightGreen"));
+                            title
+                        } else {
+                            TString::new(val.title.to_string())
+                        };
+                        let created = TString::new(val.created.to_string());
+                        let ended = TString::new(val.ended.to_string());
+                        renderer.setRow(vec![number.clone(), status, title, created, ended]);
+                    }
+                    false => {
+                        continue;
+                    }
+                }
+            }
         }
 
-        renderer.adaptColumnLengths().render();
+        renderer.adaptColumnLengths().render().flush();
 
         self
     }
@@ -203,7 +262,27 @@ impl Todo {
             .get(String::from("fileNameDB"))
             .unwrap()
             .to_string();
+
         self.fileWorker.write(fileNameDB, json_string).unwrap();
+
+        let fileNameDB: String = self
+            .settings
+            .get(String::from("fileNameDB"))
+            .unwrap()
+            .to_string();
+
+        let tmplEmpty: String = self
+            .settings
+            .template(String::from("emptyDB"))
+            .unwrap()
+            .to_string();
+
+        let content = self
+            .fileWorker
+            .fileToString(fileNameDB.clone(), tmplEmpty.clone())
+            .unwrap();
+        self.properties = Self::initProperties(content.clone());
+        self.items = Self::initItems(content.clone());
         self
     }
 
@@ -228,6 +307,10 @@ impl Todo {
         renderer.setRow(vec![
             TString::new(String::from("add")),
             TString::new(String::from("Add a task")),
+        ]);
+        renderer.setRow(vec![
+            TString::new(String::from("rm or delete or del")),
+            TString::new(String::from("Delete a task")),
         ]);
         renderer.setRow(vec![
             TString::new(String::from("push")),
@@ -257,6 +340,31 @@ impl Todo {
         self
     }
 
+    pub fn rmTaskByIndex(&mut self, indexes: Vec<String>) -> &mut Self {
+        let mut vi32ItemsForDelete: Vec<i32> = Vec::new();
+        for index in indexes {
+            let indexVal: usize = index.parse().unwrap();
+            if indexVal > self.items.len() {
+                println!("index out of range");
+                continue;
+            } else {
+                vi32ItemsForDelete.push(indexVal as i32);
+                let mut item = self.items[indexVal].clone();
+                item.params
+                    .insert(String::from("deleted"), String::from("true"));
+                self.items[indexVal] = item;
+            }
+        }
+        println!("\nThe next items is deleted:");
+        self.setTodoIndexes(vi32ItemsForDelete)
+            .show()
+            .resetTodoIndexes();
+        self.items
+            .retain(|item| !item.params.contains_key(&String::from("deleted")));
+        println!("\n\n");
+        self
+    }
+
     pub fn run(&mut self) -> &mut Self {
         let scannerRef = &self.scanner;
         match scannerRef.command.as_str() {
@@ -268,6 +376,9 @@ impl Todo {
                 .addTask(scannerRef.param.clone().as_str())
                 .sync()
                 .show(),
+            "rm" => self.rmTaskByIndex(scannerRef.params.clone()).sync().show(),
+            "delete" => self.rmTaskByIndex(scannerRef.params.clone()).sync().show(),
+            "del" => self.rmTaskByIndex(scannerRef.params.clone()).sync().show(),
             "push" => self
                 .addTask(scannerRef.param.clone().as_str())
                 .sync()
