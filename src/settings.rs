43ae6675953347f8664d22212b0a5eebb3aa6fae
silksofthesoul <silksofthesoul@yandex.ru>
2024-09-27 02:05:44 +0300

    feat(settings, static): Хранение больших файлов шаблонов и ресурсов отдельно

--- src/settings.rs
+++ src/settings.rs
@@ -1,4 +1,5 @@
 #![allow(non_snake_case)]
+use include_dir::{include_dir, Dir};
 
 use std::collections::HashMap;
 
@@ -10,14 +11,35 @@ pub struct Settings {
 
 impl Settings {
     pub fn new() -> Settings {
+        static PROJECT_DIR: Dir = include_dir!("src/static");
+
         let mut settings: HashMap<String, String> = HashMap::new();
         settings.insert(String::from("fileNameDB"), String::from("todo.json"));
+        settings.insert(String::from("mdFile"), String::from("todo.md"));
+        settings.insert(String::from("htmlFile"), String::from("todo.html"));
 
         let mut templates: HashMap<String, String> = HashMap::new();
-        templates.insert(
-            String::from("emptyDB"),
-            String::from(r#"{ "properties": { "test": "test-val" }, "items": [] }"#),
-        );
+
+        let jsonTemplate = PROJECT_DIR
+            .get_file("todo.template.json")
+            .unwrap()
+            .contents_utf8()
+            .unwrap();
+        templates.insert(String::from("emptyDB"), String::from(jsonTemplate));
+
+        let htmlTemplate = PROJECT_DIR
+            .get_file("todo.template.html")
+            .unwrap()
+            .contents_utf8()
+            .unwrap();
+        templates.insert(String::from("todoHTML"), String::from(htmlTemplate));
+
+        let cssStyles = PROJECT_DIR
+            .get_file("styles.css")
+            .unwrap()
+            .contents_utf8()
+            .unwrap();
+        templates.insert(String::from("cssStyles"), String::from(cssStyles));
 
         Settings {
             settings,
