8523f8229af27964268a616bac5557591c496fde
silksofthesoul <silksofthesoul@yandex.ru>
2024-09-27 02:07:12 +0300

    feat(markdownrender): add markdown render & export to html

--- src/markdownrender/markdownrender.rs
+++ src/markdownrender/markdownrender.rs
@@ -1,24 +1,20 @@
 use super::super::todo::TodoItem;
+use pulldown_cmark::{html, Options, Parser};
 use serde_yaml;
 use std::collections::HashMap;
 
-pub struct MarkdownRender {
-    fileName: String,
-    content: String,
-}
+pub struct MarkdownRender {}
 
 impl MarkdownRender {
-    pub fn new(fileName: &str) -> MarkdownRender {
-        MarkdownRender {
-            fileName: String::from(fileName),
-            content: String::from(""),
-        }
+    pub fn new() -> MarkdownRender {
+        MarkdownRender {}
     }
 
     pub fn todoToMarkdown(
         &mut self,
         todoProperties: HashMap<String, String>,
         todoItems: Vec<TodoItem>,
+        isHtmlRender: bool,
     ) -> String {
         let mut markdown = String::from("");
         let mut properties = String::from("");
@@ -30,6 +26,8 @@ impl MarkdownRender {
         //properties.push_str("---\n");
         properties.push_str(&yamlProperties);
         properties.push_str("---\n");
+
+        markdown.push_str(&properties);
 
         /* title: */
         let mut commonTitle: String = String::from("");
@@ -46,23 +44,65 @@ impl MarkdownRender {
         }
 
         /* items: */
+        //items.push_str("## Items\n");
+        //items.push_str("\n");
+        //    for item in todoItems {
+        //        let status = item.status;
+        //        let checkbox = if status { "[x]" } else { "[ ]" };
+        //        let title = item.title;
+        //        let created = item.created;
+        //        let ended = item.ended;
+        //        items.push_str(&format!(
+        //            "- {} {} | {} - {}\n",
+        //            checkbox, title, created, ended
+        //        ));
+        //    }
+
+        /* items: */
         items.push_str("## Items\n");
         items.push_str("\n");
+        items.push_str("| Status | Title | Created | Ended |\n");
+        items.push_str("| --- | --- | --- | --- |\n");
         for item in todoItems {
             let status = item.status;
-            let checkbox = if status { "[x]" } else { "[ ]" };
+            let mut checkbox = "";
+            if isHtmlRender {
+                checkbox = if status {
+                    "<input type=\"checkbox\" checked disabled>"
+                } else {
+                    "<input type=\"checkbox\" disabled>"
+                };
+            } else {
+                checkbox = if status { "[x]" } else { "[ ]" };
+            }
             let title = item.title;
             let created = item.created;
             let ended = item.ended;
             items.push_str(&format!(
-                "- {} {} | {} - {}\n",
+                "| {} | {} | {} | {} |\n",
                 checkbox, title, created, ended
             ));
+            //items.push_str(&format!(
+            //    "- {} {} | {} - {}\n",
+            //    checkbox, title, created, ended
+            //));
         }
 
-        markdown.push_str(&properties);
         markdown.push_str(&items);
 
         markdown
+    }
+
+    pub fn mdToHtml(&mut self, mdString: String) -> String {
+        let mut options = Options::empty();
+        options.insert(Options::ENABLE_TABLES);
+        options.insert(Options::ENABLE_TASKLISTS);
+        options.insert(Options::ENABLE_STRIKETHROUGH);
+        options.insert(Options::ENABLE_HEADING_ATTRIBUTES);
+        options.insert(Options::ENABLE_YAML_STYLE_METADATA_BLOCKS);
+        let parser = Parser::new_ext(&mdString, options);
+        let mut html_output = String::new();
+        html::push_html(&mut html_output, parser);
+        html_output
     }
 }
